bd45d1dc39efa22a4544eef65c60adec22ecb085
Aleksander Pascoal <aleksvp@MacBook-Air-de-Aleksander.local>
2020-05-20 09:45:46 -0300

    Includes export funcionality

--- src/views/SimpleTable.vue
+++ src/views/SimpleTable.vue
@@ -1,17 +1,76 @@
 <template>
-    <v-card width="600px" class="mt-5 mx-auto">
-        Uai
+    <v-card outlined class="mx-auto mt-10 primary" max-width="700">
+        <v-card-title class="white--text">Simple table</v-card-title>
+        <v-card-actions>
+            <v-divider/>
+            <v-btn @click="exportSheet">Export</v-btn>
+        </v-card-actions>
+        <v-data-table
+            show-select
+            ref="simpleTable"
+            v-model="rowsSelected"
+            class="elevation-1"
+            :headers="tableColumns"
+            :items="items"
+            item-key="product"
+            :footer-props="{
+                itemsPerPageOptions: [5, 10, 15],
+                showFirstLastPage: true,
+                firstIcon: 'mdi-arrow-collapse-left',
+                lastIcon: 'mdi-arrow-collapse-right',
+                prevIcon: 'mdi-arrow-left',
+                nextIcon: 'mdi-arrow-right'}"
+            :search="searchKey">
+        </v-data-table>
     </v-card>
 </template>
 
 <script>
+import XLSX from 'xlsx'
 
 export default {
     data() {
         return {
+            searchKey: '',
+            simpleTable: {},
+            rowsSelected: [],
+            tableColumns:[
+                {
+                    text: 'Product',
+                    value: 'product'
+                },
+                {
+                    text: 'Quantity',
+                    value: 'quantity'
+                 },
+                 {
+                     text: 'Origin',
+                     value: 'origin'
+                 } 
+            ],
+            items: [
+                { product: 'Apples', quantity: 1000, origin: 'Ceilândia'}
+            ],
         }
     },
     methods: {
+        exportSheet(){
+            let header = this.tableColumns.map(element => element.value)
+            var dataRows = []
+            this.rowsSelected.map( row => {
+                var dataRow = []
+                
+                header.map(column => {
+                    dataRow.push(row[column])
+                })
+                dataRows.push(dataRow)
+            })
+            console.log(dataRows)
+            const ws = XLSX.utils.aoa_to_sheet([header,... dataRows])
+			const wb = XLSX.utils.book_new()
+			XLSX.utils.book_append_sheet(wb, ws, "SimpleTab")
+			XLSX.writeFile(wb, "Example.xlsx")
+        },
     },
 }
 </script>
