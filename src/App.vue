158f6fa980e96cebf0c749d6cd7d59eaef20202b
Aleksander Pascoal <aleksvp@MacBook-Air-de-Aleksander.local>
2020-05-20 08:11:24 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <simpletable/>
+  </v-app>
+</template>
+
+<script>
+import SimpleTable from './views/SimpleTable.vue'
+
+export default {
+    components: {
+      'simpletable': SimpleTable
+    }, 
+}
+</script>
---
+++ src/views/SimpleTable.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-card width="600px" class="mt-5 mx-auto">
+        Uai
+    </v-card>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+        }
+    },
+    methods: {
+    },
+}
+</script>
